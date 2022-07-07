8d11a201e5c0056a3c370faaac3720655fceec60
Karthic  Shanmugam <68475401+karthic82@users.noreply.github.com>
2022-07-07 17:07:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,23 +22,7 @@ module "account-standardizations"{
   LogsRetentionInDays = var.LogsRetentionInDays
 }
 
-module "account-standardizations"{
-  source = "./modules/account-standardizations"
-  PermissionsBoundary = var.PermissionsBoundary
-  AllowUsersToChangePassword = var.AllowUsersToChangePassword
-  HardExpiry = var.HardExpiry
-  MaxPasswordAge = var.MaxPasswordAge
-  MinimumPasswordLength = var.MinimumPasswordLength
-  PasswordReusePrevention = var.PasswordReusePrevention
-  RequireLowercaseCharacters = var.RequireLowercaseCharacters
-  RequireNumbers = var.RequireNumbers
-  RequireSymbols = var.RequireSymbols
-  RequireUppercaseCharacters = var.RequireUppercaseCharacters
-  LogsRetentionInDays = var.LogsRetentionInDays
-}
-
   module "log-management" {
     source = "./modules/log-management"
-
-  }
+}
     
