63ba78c84b509bf8c6a2956dc3d83c7b707c123e
Karthic  Shanmugam <68475401+karthic82@users.noreply.github.com>
2022-07-07 16:15:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,10 @@ provider "aws" {
   region  = var.region
 }
 
-module "account-standardizations"{
-  source = "./modules/account-standardizations"
-  PermissionsBoundary = var.PermissionsBoundary
-  AllowUsersToChangePassword = var.AllowUsersToChangePassword
-  HardExpiry = var.HardExpiry
-  MaxPasswordAge = var.MaxPasswordAge
-  MinimumPasswordLength = var.MinimumPasswordLength
-  PasswordReusePrevention = var.PasswordReusePrevention
-  RequireLowercaseCharacters = var.RequireLowercaseCharacters
-  RequireNumbers = var.RequireNumbers
-  RequireSymbols = var.RequireSymbols
-  RequireUppercaseCharacters = var.RequireUppercaseCharacters
-  LogsRetentionInDays = var.LogsRetentionInDays
-}
+  module "log-management" {
+    source = "./modules/log-management"
+    OperatorEmail = var.OperatorEmail
 
+  }
+    
     
